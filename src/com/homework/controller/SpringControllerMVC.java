0d1dec00cd86494745e9f74a0a331b963e2a53a0
Lrakulka <olecsandr32@gmail.com>
2016-05-22 11:31:39 +0300

    Loggin page added

--- src/com/homework/controller/SpringControllerMVC.java
+++ src/com/homework/controller/SpringControllerMVC.java
@@ -36,6 +36,24 @@ public class SpringControllerMVC {
 	return model;
     }
     
+  //Spring Security see this :
+  @RequestMapping(value = "/login", method = RequestMethod.GET)
+  public ModelAndView login(
+  	@RequestParam(value = "error", required = false) String error,
+  	@RequestParam(value = "logout", required = false) String logout) {
+
+  	ModelAndView model = new ModelAndView();
+  	if (error != null) {
+  	    model.addObject("error", "Invalid username and password!");
+  	}
+
+  	if (logout != null) {
+  	    model.addObject("msg", "You've been logged out successfully.");
+  	}
+  	model.setViewName("login");
+  	return model;
+  }
+  	
     @RequestMapping(value = "/admin", method = RequestMethod.GET)
     public ModelAndView adminHomePage() {
 	ModelAndView model = new ModelAndView();
