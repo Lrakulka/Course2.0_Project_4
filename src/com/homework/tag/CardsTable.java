cfe4384c2718d0ca2abc14f81938f259e8352360
Lrakulka <olecsandr32@gmail.com>
2016-05-25 11:04:57 +0300

    Added logger.

--- src/com/homework/tag/CardsTable.java
+++ src/com/homework/tag/CardsTable.java
@@ -3,16 +3,19 @@
  */
 package com.homework.tag;
 
+import java.io.IOException;
 import java.util.List;
 import javax.servlet.jsp.tagext.TagSupport;
+import org.apache.log4j.Logger;
 import com.homework.entities.Card;
 
 /**
  * @author asd
- *
+ * Handling tag cardsTable
  */
 public class CardsTable extends TagSupport {
     private static final long serialVersionUID = -3905900903200282870L;
+    private static final Logger logger = Logger.getLogger(CardsTable.class);  
     private String buttonFillInfo;
     private String buttonMakePaymentInfo;
     private String buttonBlockInfo;
@@ -54,51 +57,54 @@ public class CardsTable extends TagSupport {
 	this.cards = cards;
     }
     
+    @Override
     public int doStartTag() {
+	logger.info("Entering");
 	StringBuilder tableBuilder = new StringBuilder(
 		"<table border=\"2\" cellpadding=\"8\">");
-	try {
-	    for (Card card : cards) {
-		tableBuilder.append("<tr><td>" + card.getName() + 
-			"</td><td>" +
-			"<p align=\"center\">" + card.getBill().getScore() + "</p></td>" +
-			"<td><form name=\"CardTable\" action=\"/Project_4/fillClientBill\" " +
-			"method=\"post\">" +		
-			"<input type=\"hidden\" name=\"" + parameterName +"\"" +
-			"	value=\"" + token + "\" />" +
-			"<p align=\"center\"><input size=\"10\" type=\"text\" name=\"moneyCount\" /></p>" +
-			"<p align=\"center\"><button name=\"fillBill\" value=\"" + 
-			card.getId() + "\">" + buttonFillInfo + 
-			"</button></form></td>" + 
-			"<td><form name=\"CardTable\" action=\"/Project_4/makeClientPayment\" " +
-			"method=\"post\">" +	
-			"<input type=\"hidden\" name=\"" + parameterName +"\"" +
-			"	value=\"" + token + "\" />" +
-			"<p align=\"center\"><input size=\"10\" type=\"text\" name=\"moneyCount\" /></p>" +
-			"<p align=\"center\"><button name=\"makePayment\" value=\"" + 
-			card.getId() + "\">" + buttonMakePaymentInfo + 
-			"</button></form></td></p>" +
-			"</td><td><p>" + (card.getBill().getIsBlocked() ? 
-				textBlocked : textUnBlocked));
-			if (!card.getBill().getIsBlocked()) {
-			    tableBuilder.append("<form name=\"CardTable\" " +
-			    		"action=\"/Project_4/blockClientBill\" " +
-			    		"method=\"post\">" + 
-			    		"<input type=\"hidden\" name=\"" + 
-			    		parameterName +"\" value=\"" + token + "\" />" + 
-			    		"</p><p align=\"center\">" +
-					"<button name=\"blockBill\" value=\"" + 
-					card.getId() + "\">" + buttonBlockInfo + 
-					"</button></p></form></td>");	
-			}
-		tableBuilder.append("</tr>");
+	for (Card card : cards) {
+	    tableBuilder.append("<tr><td>" + card.getName() + 
+		"</td><td>" +
+		"<p align=\"center\">" + card.getBill().getScore() + "</p></td>" +
+		"<td><form name=\"CardTable\" action=\"/Project_4/fillClientBill\" " +
+		"method=\"post\">" +		
+		"<input type=\"hidden\" name=\"" + parameterName +"\"" +
+		"	value=\"" + token + "\" />" +
+		"<p align=\"center\"><input size=\"10\" type=\"text\" name=\"moneyCount\" /></p>" +
+		"<p align=\"center\"><button name=\"fillBill\" value=\"" + 
+		card.getId() + "\">" + buttonFillInfo + 
+		"</button></form></td>" + 
+		"<td><form name=\"CardTable\" action=\"/Project_4/makeClientPayment\" " +
+		"method=\"post\">" +	
+		"<input type=\"hidden\" name=\"" + parameterName +"\"" +
+		"	value=\"" + token + "\" />" +
+		"<p align=\"center\"><input size=\"10\" type=\"text\" name=\"moneyCount\" /></p>" +
+		"<p align=\"center\"><button name=\"makePayment\" value=\"" + 
+		card.getId() + "\">" + buttonMakePaymentInfo + 
+		"</button></form></td></p>" +
+		"</td><td><p>" + (card.getBill().getIsBlocked() ? 
+			textBlocked : textUnBlocked));
+	    if (!card.getBill().getIsBlocked()) {
+		    tableBuilder.append("<form name=\"CardTable\" " +
+		    		"action=\"/Project_4/blockClientBill\" " +
+		    		"method=\"post\">" + 
+		    		"<input type=\"hidden\" name=\"" + 
+		    		parameterName +"\" value=\"" + token + "\" />" + 
+		    		"</p><p align=\"center\">" +
+				"<button name=\"blockBill\" value=\"" + 
+				card.getId() + "\">" + buttonBlockInfo + 
+				"</button></p></form></td>");	
 	    }
-	    tableBuilder.append("</table>");
-	    pageContext.getOut().write(tableBuilder.toString());
-	} catch (Exception e) {
-	   // LOGGER.error("Problem in lecture table(tag)", e);
-	    
+	    tableBuilder.append("</tr>");
 	}
+	tableBuilder.append("</table>");
+	try {
+	    pageContext.getOut().write(tableBuilder.toString());
+	} catch (IOException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
+	logger.info("Leaving actorName=");
 	return SKIP_BODY;
     }
 }
--- src/com/homework/tag/ClientsTable.java
+++ src/com/homework/tag/ClientsTable.java
@@ -2,19 +2,23 @@
  * 
  */
 package com.homework.tag;
+import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.jsp.tagext.TagSupport;
+
+import org.apache.log4j.Logger;
 
 import com.homework.entities.Actor;
 import com.homework.entities.Card;
 
 /**
  * @author asd
- *
+ * Handling tag clientTable
  */
 public class ClientsTable extends TagSupport {
     private static final long serialVersionUID = 8721266929256929800L;
+    private static final Logger logger = Logger.getLogger(ClientsTable.class);  
     private String buttonInfo;
     private List<Actor> clients;
     private String parameterName;
@@ -47,49 +51,51 @@ public class ClientsTable extends TagSupport {
     }
     
     public int doStartTag() {
+	logger.info("Entering");
 	StringBuilder tableBuilder = new StringBuilder(
 		"<form name=\"ClientTable\" action=\"/Project_4/releaseClientBill\" " +
 			"method=\"post\">" + "<table border=\"2\" cellpadding=\"8\">" +
 			"<input type=\"hidden\" name=\"" + parameterName +"\"" +
 			"	value=\"" + token + "\" />");
 	int i;
-	try {
-	    for (Actor actor : clients) {
-		i = 0;
-		if (actor.getCards().size() > 1) {
-		    tableBuilder.append("<tr><td rowspan=" + 
-			    actor.getCards().size() + ">" + 
-			    actor.getName() + "</td>");
-		} else {
-		    tableBuilder.append("<tr><td>" + actor.getName() + 
+	for (Actor actor : clients) {
+	    i = 0;
+	    if (actor.getCards().size() > 1) {
+		tableBuilder.append("<tr><td rowspan=" + 
+			actor.getCards().size() + ">" + 
+			actor.getName() + "</td>");
+	    } else {
+		tableBuilder.append("<tr><td>" + actor.getName() + 
 			    "</td>");
+	    }
+	    for (Card card : actor.getCards()) {
+		if (i > 0) {
+		    tableBuilder.append("<tr>");
 		}
-		for (Card card : actor.getCards()) {
-		    if (i > 0) {
-			tableBuilder.append("<tr>");
-		    }
-		    tableBuilder.append("<td>" + card.getName() +
+		tableBuilder.append("<td>" + card.getName() +
 			"</td><td>" + card.getBill().getScore() + 
 			"</td><td>" + (card.getBill().getIsBlocked() ? textBlocked :
 			    textUnBlocked) + "</td>");
-		    if (card.getBill().getIsBlocked()) {
+		if (card.getBill().getIsBlocked()) {
 			tableBuilder.append("<td><button name=\"billBlockId\" value=\"" +
 				card.getBill().getId() +
 				"\">" + buttonInfo + "</button></td>");
-		    }
-		    i++;
-		    if ((i > 0) && (i != actor.getCards().size())) {
-			tableBuilder.append("</tr>");
-		    }
 		}
-		tableBuilder.append("</tr>");
+		i++;
+		if ((i > 0) && (i != actor.getCards().size())) {
+			tableBuilder.append("</tr>");
+		}
 	    }
-	    tableBuilder.append("</table></form>");
-	    pageContext.getOut().write(tableBuilder.toString());
-	} catch (Exception e) {
-	   // LOGGER.error("Problem in lecture table(tag)", e);
-	    
+	tableBuilder.append("</tr>");
 	}
+	tableBuilder.append("</table></form>");
+	try {
+	    pageContext.getOut().write(tableBuilder.toString());
+	} catch (IOException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
+	logger.info("Leaving actorName=");
 	return SKIP_BODY;
     }
 }
